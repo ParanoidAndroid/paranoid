2262fffcee57e4680d8acab109e2a2d3b789a5ac
D4rKn3sSyS <jesuelfather@hotmail.com>
2012-06-18 23:03:36 -0500

    Removed spaces on total time elapsed

--- build.sh
+++ build.sh
@@ -76,4 +76,4 @@ echo -e ""
 
 # finished? get elapsed time
 res2=$(date +%s.%N)
-echo "${bldgrn}Total time elapsed:    $(echo "($res2 - $res1)/60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
+echo "${bldgrn}Total time elapsed: $(echo "($res2 - $res1)/60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
