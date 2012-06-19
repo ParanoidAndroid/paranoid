18f16d482578261968e933dcdbcc26359797eb13
D4rKn3sSyS <jesuelfather@hotmail.com>
2012-06-19 11:07:29 -0500

    Add get prebuilts script

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Colorize and add text parameters
-txtund=$(tput sgr 0 1)          # Underline
+red=$(tput setaf 1)             #  red
+grn=$(tput setaf 2)             #  green
+cya=$(tput setaf 6)             #  cyan
 txtbld=$(tput bold)             # Bold
 bldred=${txtbld}$(tput setaf 1) #  red
 bldgrn=${txtbld}$(tput setaf 2) #  green
 bldblu=${txtbld}$(tput setaf 4) #  blue
 bldcya=${txtbld}$(tput setaf 6) #  cyan
-bldwht=${txtbld}$(tput setaf 7) #  white
 txtrst=$(tput sgr0)             # Reset
 
 ARGUMENTS="$1"
@@ -30,32 +31,40 @@ case "$ARGUMENTS" in
        densitytrigger="128"
        device="galaxys2"
        manufacturer="samsung"
-       echo -e "${bldcya}Building ${bldgrn}ParanoidAndroid ${bldcya}for International Samsung Galaxy S2 ${txtrst}";;
+       echo -e "${cya}Building ${bldcya}ParanoidAndroid ${txtrst}${cya}for International Samsung Galaxy S2 ${txtrst}";;
    maguro)
        densitytrigger="192"
        device="maguro"
        manufacturer="samsung"
-       echo -e "${bldcya}Building ${bldgrn}ParanoidAndroid ${bldcya}for International Samsung Galaxy Nexus ${txtrst}";;
+       echo -e "${cya}Building ${bldcya}ParanoidAndroid ${txtrst}${cya}for International Samsung Galaxy Nexus ${txtrst}";;
    galaxys3)
        densitytrigger="192"
        device="i9300"
        manufacturer="samsung"
-       echo -e "${bldcya}Building ${bldgrn}ParanoidAndroid ${bldcya}for International Samsung Galaxy S3 ${txtrst}";;
+       echo -e "${cya}Building ${bldcya}ParanoidAndroid ${txtrst}${cya}for International Samsung Galaxy S3 ${txtrst}";;
    toro)
        densitytrigger="192"
        device="toro"
        manufacturer="samsung"
-       echo -e "${bldcya}Building ${bldgrn}ParanoidAndroid ${bldcya}for Verizon Samsung Galaxy Nexus ${txtrst}";;
+       echo -e "${cya}Building ${bldcya}ParanoidAndroid ${txtrst}${cya}for Verizon Samsung Galaxy Nexus ${txtrst}";;
    toroplus)
        densitytrigger="192"
        device="toroplus"
        manufacturer="samsung"
-       echo -e "${bldcya}Building ${bldgrn}ParanoidAndroid ${bldcya}for Sprint Samsung Galaxy Nexus ${txtrst}";;
+       echo -e "${cya}Building ${bldcya}ParanoidAndroid ${txtrst}${cya}for Sprint Samsung Galaxy Nexus ${txtrst}";;
    *)
        echo -e "${bldred}Please input device name ${txtrst}"
        exit;;
 esac
 
+echo -e ""
+# download prebuilt files
+echo -e "${bldblu}Downloading prebuilts ${txtrst}"
+cd vendor/cm
+./get-prebuilts
+cd ./../..
+
+echo -e ""
 # change density for trigger on tablet mode
 echo -e "${bldblu}Overwriting density for trigger density ${txtrst}"
 cd device/$manufacturer/$device/
@@ -94,4 +103,4 @@ echo -e ""
 
 # finished? get elapsed time
 res2=$(date +%s.%N)
-echo "${bldgrn}Total time elapsed: $(echo "($res2 - $res1)/60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
+echo "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1)/60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
