69fbea765c70e17e73cb6cb61501794289be15cd
D4rKn3sSyS <jesuelfather@hotmail.com>
2012-07-12 13:02:46 -0500

    Automatically fetch device tree and append into local_manifest
    Change-Id: I4ff96ad1bb594d12bd8d8149896a13aee8972359

--- build.sh
+++ build.sh
@@ -85,6 +85,10 @@ case "$DEVICE" in
        exit;;
 esac
 
+echo -e "${cya}"
+./vendor/pa/tools/getdevicetree.py $device
+echo -e "${txtrst}"
+
 # decide what command to execute
 case "$EXTRAS" in
    threads)
